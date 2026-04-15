d4477e2f4e87148dc63801687c69618dd729d86b
camila-guimaraes <camilatgui@gmail.com>
2026-04-15 20:51:59 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -17,3 +17,8 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+numero= float(input("Digite um número:"))
+if numero % 2 == 0:
+    print ("Par")
+else:
+    print("Ímpar")
